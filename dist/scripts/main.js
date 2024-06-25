5da8859b4f4e2efbd7c4ca8a532252e4976c698e
Angelaqueb <angela.quebriac@gmail.com>
2024-06-25 12:25:04 +0200

    sauvegarde

---
+++ dist/scripts/main.js
@@ -0,0 +1,9 @@
+const menu =  document.querySelector('.nav_menuResponsive')
+const burgerButton = document.querySelector('.burger_icon')
+
+burgerButton.addEventListener('click', function(){
+    console.log('click')
+    menu.classList.toggle('active')
+})
+
+//_________________________________________________________
