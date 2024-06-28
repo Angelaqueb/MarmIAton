ea0f1b0970e76c34a592d35f6ac60a1d38326bf8
Angelaqueb <angela.quebriac@gmail.com>
2024-06-28 16:57:09 +0200

    pop in

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -6,6 +6,55 @@ burgerButton.addEventListener('click', function(){
     menu.classList.toggle('active')
 })
 
+//_______________________pop in__________________________________
+
+
+const popin =  document.querySelector('.popin')
+const buttonVoice = document.querySelector('.news_1')
+const closeVoice = document.querySelector('.popin_container1_X')
+const closeVoice2 = document.querySelector('.popin_container2_button')
+
+
+buttonVoice.addEventListener('click', function(){
+    console.log('click')
+    popin.classList.toggle('active')
+})
+
+
+closeVoice.addEventListener('click', function(){
+    console.log('click')
+    popin.classList.remove('active')
+})
+
+closeVoice2.addEventListener('click', function(){
+    console.log('click')
+    popin.classList.remove('active')
+})
+
+//_______________________pop in2__________________________________
+
+
+const popin2 =  document.querySelector('.popin2')
+const buttonConnexion = document.querySelector('.news_2')
+const closeConnexion = document.querySelector('.popin2_container1_X')
+const closeConnexion2 = document.querySelector('.input2')
+
+
+buttonConnexion.addEventListener('click', function(){
+    console.log('click')
+    popin2.classList.toggle('active')
+})
+
+
+closeConnexion.addEventListener('click', function(){
+    console.log('click')
+    popin2.classList.remove('active')
+})
+closeConnexion2.addEventListener('click', function(){
+    console.log('click')
+    popin2.classList.remove('active')
+})
+
 //_______________________Page recette  slide __________________________________
 
 document.addEventListener('DOMContentLoaded', function() {
